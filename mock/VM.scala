0c9fe9ae3888b6726f513971561990e8a85ea340
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2023-06-29 22:19:01 +0700

    WE-8633 - Fix bytecode

--- mock/VM.scala
+++ mock/VM.scala
@@ -25,7 +25,7 @@ object VM {
         // (module
         //   (type $t0 (func (param i32 i32 i32 i32 i32 i32) (result i32)))
         //   (type $t1 (func (result i32)))
-        //   (import "env" "call_contract" (func $call (type $t0)))
+        //   (import "env0" "call_contract" (func $call (type $t0)))
         //   (import "env" "memory" (memory $env.memory 1 1))
         //   (func $run (export "run") (type $t1) (result i32)
         //     (call $call
@@ -41,28 +41,33 @@ object VM {
         //   (data $d2 (i32.const 6) "\01\02\03\04")
         // )
         val bytecode = Array[Byte](
-            0, 97, 115, 109, 1, 0, 0, 0, 1, 15, 2, 96, 6, 127, 127, 127, 127, 127, 127,
-            1, 127, 96, 0, 1, 127, 2, 36, 2, 3, 101, 110, 118, 13, 99, 97, 108, 108, 95,
+            0, 97, 115, 109, 1, 0, 0, 0, 1, 15, 2, 96, 6, 127, 127, 127, 127, 127, 127, 
+            1, 127, 96, 0, 1, 127, 2, 37, 2, 4, 101, 110, 118, 48, 13, 99, 97, 108, 108, 95, 
             99, 111, 110, 116, 114, 97, 99, 116, 0, 0, 3, 101, 110, 118, 6, 109, 101, 109,
             111, 114, 121, 2, 1, 1, 1, 3, 2, 1, 1, 7, 7, 1, 3, 114, 117, 110, 0, 1, 10, 18,
             1, 16, 0, 65, 0, 65, 3, 65, 3, 65, 3, 65, 6, 65, 4, 16, 0, 11, 11, 26, 3, 0, 65,
             0, 11, 3, 116, 119, 111, 0, 65, 3, 11, 3, 114, 117, 110, 0, 65, 6, 11, 4, 1, 2,
-            3, 4, 0, 14, 4, 110, 97, 109, 101, 1, 7, 1, 0, 4, 99, 97, 108, 108
+            3, 4, 0, 60, 4, 110, 97, 109, 101, 1, 12, 2, 0, 4, 99, 97, 108, 108, 1, 3, 114,
+            117, 110, 4, 9, 2, 0, 2, 116, 48, 1, 2, 116, 49, 6, 13, 1, 0, 10, 101, 110, 118,
+            46, 109, 101, 109, 111, 114, 121, 9, 13, 3, 0, 2, 100, 48, 1, 2, 100, 49, 2, 2,
+            100, 50
         )
+
 
         val wrongBytecode = Array[Byte](
             0, 14, 21, 1, 2
         )
+
         val funcName = "run"
         val funcArgs = Array[String]()
 
-        val vm1 = new VM
-        val isCorrect = vm1.validateBytecode(bytecode) == 0
-        val isIncorrect = vm1.validateBytecode(wrongBytecode) == -1
-        val vm2 = new VM
+        val vm = new VM
+        val isCorrect = vm.validateBytecode(bytecode) == 0
+        val isIncorrect = vm.validateBytecode(wrongBytecode) == -1
         println(s"bytecode is correct: $isCorrect")
         println(s"wrong bytecode is incorrect: $isIncorrect")
-        val result = vm2.runContract(bytecode, funcName, funcArgs, new VM)
+
+        val result = vm.runContract(bytecode, funcName, funcArgs, new VM)
         println(s"run contract result: $result")
     }
 }
