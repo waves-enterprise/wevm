771a2ca4444c6ba5341fedf889509d8c840d0bd9
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2024-02-06 00:28:24 +0700

    WE-8811 Changed environment

--- native/src/env.rs
+++ native/src/env.rs
@@ -1,35 +1,8 @@
-mod asset;
-mod block;
-mod call_contract;
-mod lease;
-mod payments;
-mod storage;
-mod tx;
-mod utils;
-
-use crate::runtime::Runtime;
-use dyn_clone::DynClone;
-use wasmi::{Func, Store};
-
-pub trait Environment: DynClone {
-    fn module(&self) -> String;
-    fn name(&self) -> String;
-    fn func(&self, store: &mut Store<Runtime>) -> Func;
-}
-
-dyn_clone::clone_trait_object!(Environment);
-
-pub fn envs() -> Vec<Box<dyn Environment>> {
-    let mut result: Vec<Box<dyn Environment>> = vec![];
-
-    result.extend(asset::to_vec());
-    result.extend(block::to_vec());
-    result.extend(call_contract::to_vec());
-    result.extend(lease::to_vec());
-    result.extend(payments::to_vec());
-    result.extend(storage::to_vec());
-    result.extend(tx::to_vec());
-    result.extend(utils::to_vec());
-
-    result
-}
+pub mod asset;
+pub mod block;
+pub mod call_contract;
+pub mod lease;
+pub mod payments;
+pub mod storage;
+pub mod tx;
+pub mod utils;
--- native/src/env/lease.rs
+++ native/src/env/lease.rs
@@ -1,89 +1,59 @@
-use crate::{
-    env::Environment, env_items, env_runtime, error::RuntimeError, node::Node, runtime::Runtime,
-    write_memory,
-};
-use convert_case::{Case, Casing};
-use wasmi::{Caller, Func, Store};
+use crate::{error::RuntimeError, node::Node, runtime::Runtime, write_memory};
+use wasmi::Caller;
 
-env_items!(LeaseAddress, LeaseAlias, CancelLease);
+pub fn lease(
+    version: u32,
+    offset: u32,
+    length: u32,
+    amount: i64,
+    mut caller: Caller<Runtime>,
+) -> (i32, i32, i32) {
+    let (memory, ctx) = match caller.data().memory() {
+        Some(memory) => memory.data_and_store_mut(&mut caller),
+        None => return (RuntimeError::MemoryNotFound as i32, 0, 0),
+    };
+    let offset_memory = ctx.heap_base() as usize;
 
-env_runtime! {
-    #[version = 0]
-    pub fn LeaseAddress(
-        offset_address: u32,
-        length_address: u32,
-        amount: i64,
-    ) -> (i32, i32, i32) {
-        |mut caller: Caller<Runtime>| {
-            let (memory, ctx) = match caller.data().memory() {
-                Some(memory) => memory.data_and_store_mut(&mut caller),
-                None => return (RuntimeError::MemoryNotFound as i32, 0, 0),
-            };
-            let offset_memory = ctx.heap_base() as usize;
+    let contract_id = ctx.vm.top_frame().contract_id();
+    let value = &memory[offset as usize..offset as usize + length as usize];
 
-            let contract_id = ctx.vm.top_frame().contract_id();
-            let address = &memory[offset_address as usize..offset_address as usize + length_address as usize];
-
-            match ctx.vm.lease(contract_id.as_slice(), address, amount) {
-                Ok(result) => write_memory!(ctx, memory, offset_memory, result),
-                Err(error) => (error.as_i32(), 0, 0),
-            }
+    let bytes = if version == 1 {
+        value.to_vec()
+    } else {
+        let mut result: Vec<u8> = vec![2];
+        match ctx.vm.get_chain_id() {
+            Ok(chain_id) => result.push(chain_id as u8),
+            Err(error) => return (error.as_i32(), 0, 0),
         }
+        result.extend_from_slice(value);
+        result
+    };
+
+    match ctx
+        .vm
+        .lease(contract_id.as_slice(), bytes.as_slice(), amount)
+    {
+        Ok(result) => write_memory!(ctx, memory, offset_memory, result),
+        Err(error) => (error.as_i32(), 0, 0),
     }
 }
 
-env_runtime! {
-    #[version = 0]
-    pub fn LeaseAlias(
-        offset_alias: u32,
-        length_alias: u32,
-        amount: i64,
-    ) -> (i32, i32, i32) {
-        |mut caller: Caller<Runtime>| {
-            let (memory, ctx) = match caller.data().memory() {
-                Some(memory) => memory.data_and_store_mut(&mut caller),
-                None => return (RuntimeError::MemoryNotFound as i32, 0, 0),
-            };
-            let offset_memory = ctx.heap_base() as usize;
+pub fn cancel_lease(
+    offset_lease_id: u32,
+    length_lease_id: u32,
+    mut caller: Caller<Runtime>,
+) -> i32 {
+    let (memory, ctx) = match caller.data().memory() {
+        Some(memory) => memory.data_and_store_mut(&mut caller),
+        None => return RuntimeError::MemoryNotFound as i32,
+    };
 
-            let contract_id = ctx.vm.top_frame().contract_id();
-            let alias = &memory[offset_alias as usize..offset_alias as usize + length_alias as usize];
+    let contract_id = ctx.vm.top_frame().contract_id();
+    let lease_id =
+        &memory[offset_lease_id as usize..offset_lease_id as usize + length_lease_id as usize];
 
-            let mut bytes: Vec<u8> = vec![];
-            bytes.push(2);
-            match ctx.vm.get_chain_id() {
-                Ok(chain_id) => bytes.push(chain_id as u8),
-                Err(error) => return (error.as_i32(), 0, 0),
-            }
-            bytes.extend_from_slice(alias);
-
-            match ctx.vm.lease(contract_id.as_slice(), bytes.as_slice(), amount) {
-                Ok(result) => write_memory!(ctx, memory, offset_memory, result),
-                Err(error) => (error.as_i32(), 0, 0),
-            }
-        }
-    }
-}
-
-env_runtime! {
-    #[version = 0]
-    pub fn CancelLease(
-        offset_lease_id: u32,
-        length_lease_id: u32,
-    ) -> i32 {
-        |mut caller: Caller<Runtime>| {
-            let (memory, ctx) = match caller.data().memory() {
-                Some(memory) => memory.data_and_store_mut(&mut caller),
-                None => return RuntimeError::MemoryNotFound as i32,
-            };
-
-            let contract_id = ctx.vm.top_frame().contract_id();
-            let lease_id = &memory[offset_lease_id as usize..offset_lease_id as usize + length_lease_id as usize];
-
-            match ctx.vm.cancel_lease(contract_id.as_slice(), lease_id) {
-                Ok(_) => 0,
-                Err(error) => error.as_i32(),
-            }
-        }
+    match ctx.vm.cancel_lease(contract_id.as_slice(), lease_id) {
+        Ok(_) => 0,
+        Err(error) => error.as_i32(),
     }
 }
