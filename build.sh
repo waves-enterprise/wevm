25e65cdb8bca500b3a15a9c6074f367395d87acf
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2024-03-02 22:13:24 +0700

    WE-8836 Update CI

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /root/.sbt
-touch /root/.sbt/.credentials
-echo "$SBT_CREDENTIALS" > /root/.sbt/.credentials
-unset SBT_CREDENTIALS
-sbt -mem 2048 "clean; buildWAT; test; assembly; publish"
